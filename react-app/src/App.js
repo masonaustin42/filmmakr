ca4c60b71607cfb3ed038ac30e3c3f5988cea809
masonaustin42 <masonaustin42@gmail.com>
2023-11-29 18:32:53 -0800

    create landing page component

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,10 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { Route, Switch } from "react-router-dom";
-import SignupFormPage from "./components/SignupFormPage";
-import LoginFormPage from "./components/LoginFormPage";
 import { authenticate } from "./store/session";
 import Navigation from "./components/Navigation";
+import LandingPage from "./components/LandingPage";
 
 function App() {
   const dispatch = useDispatch();
@@ -18,11 +17,8 @@ function App() {
       <Navigation isLoaded={isLoaded} />
       {isLoaded && (
         <Switch>
-          <Route path="/login" >
-            <LoginFormPage />
-          </Route>
-          <Route path="/signup">
-            <SignupFormPage />
+          <Route exact path="/">
+            <LandingPage />
           </Route>
         </Switch>
       )}
