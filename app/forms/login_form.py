03e0f6fe497f9e17cd6e92ed039b8412a7c0a461
masonaustin42 <masonaustin42@gmail.com>
2023-11-29 16:32:30 -0800

    change login validation to allow login with email OR username

--- app/forms/login_form.py
+++ app/forms/login_form.py
@@ -2,21 +2,33 @@ from flask_wtf import FlaskForm
 from wtforms import StringField
 from wtforms.validators import DataRequired, Email, ValidationError
 from app.models import User
+import re
 
 
 def user_exists(form, field):
     # Checking if user exists
-    email = field.data
-    user = User.query.filter(User.email == email).first()
+    credential = field.data
+    # Check if credential is email
+    if re.match(r"^[\w\d]+@[\w\d\.]+\.[\w\d]+$", credential):
+        user = User.query.filter(User.email == credential).first()
+    else:
+        user = User.query.filter(User.username == credential).first()
+        
+        
     if not user:
-        raise ValidationError('Email provided not found.')
+        raise ValidationError('Email or Username provided not found.')
 
 
 def password_matches(form, field):
     # Checking if password matches
     password = field.data
-    email = form.data['email']
-    user = User.query.filter(User.email == email).first()
+    credential = form.data['credential']
+    # Check if credential is email
+    if re.match(r"^[\w\d]+@[\w\d\.]+\.[\w\d]+$", credential):
+        user = User.query.filter(User.email == credential).first()
+    else:
+        user = User.query.filter(User.username == credential).first()
+        
     if not user:
         raise ValidationError('No such user exists.')
     if not user.check_password(password):
@@ -24,5 +36,5 @@ def password_matches(form, field):
 
 
 class LoginForm(FlaskForm):
-    email = StringField('email', validators=[DataRequired(), user_exists])
+    credential = StringField('credential', validators=[DataRequired(), user_exists])
     password = StringField('password', validators=[DataRequired(), password_matches])
