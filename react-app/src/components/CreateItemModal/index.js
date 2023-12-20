08930daa20c9c149ef733f5bce32541b02bff05a
Mason Austin <135888049+masonaustin42@users.noreply.github.com>
2023-12-20 08:18:02 -0800

    Revert "Dev"

--- react-app/src/components/CreateItemModal/index.js
+++ react-app/src/components/CreateItemModal/index.js
@@ -2,7 +2,6 @@ import { useDispatch } from "react-redux";
 import { uploadItem } from "../../store/gallery";
 import { useModal } from "../../context/Modal";
 import { useEffect, useState } from "react";
-import { socket } from "../../socket";
 import "./CreateItem.css";
 
 function CreateItemModal({ galleryId }) {
@@ -13,30 +12,6 @@ function CreateItemModal({ galleryId }) {
   const [isMain, setIsMain] = useState(false);
   const [localFile, setLocalFile] = useState(null);
   const [errors, setErrors] = useState({});
-  const [uploadProgress, setUploadProgress] = useState(0);
-  const [smoothProgress, setSmoothProgress] = useState(0);
-
-  socket.on("progress", function (data) {
-    setUploadProgress(data.percentage.toFixed());
-  });
-
-  useEffect(() => {
-    if (uploadProgress >= 100) {
-      closeModal();
-    }
-
-    const smooth = setInterval(() => {
-      if (smoothProgress < uploadProgress) {
-        if (file?.type.includes("video")) {
-          setSmoothProgress((prev) => prev + 0.1);
-        } else {
-          setSmoothProgress((prev) => prev + 1);
-        }
-      }
-    }, 10);
-
-    return () => clearInterval(smooth);
-  }, [uploadProgress]);
 
   useEffect(() => {
     const errorsObj = {};
@@ -53,6 +28,9 @@ function CreateItemModal({ galleryId }) {
     formdata.append("is_main", isMain);
     const newItem = await dispatch(uploadItem(formdata, galleryId));
     if (newItem?.errors) setErrors(newItem.errors);
+    else {
+      closeModal();
+    }
   };
 
   const onFileChange = (e) => {
@@ -101,77 +79,59 @@ function CreateItemModal({ galleryId }) {
 
   return (
     <>
-      {uploadProgress < 1 ? (
-        <>
-          <h2>Upload an Item</h2>
-          <form onSubmit={onSubmit} encType="multipart/formdata">
-            <label>
-              Name
-              <input
-                type="text"
-                value={name}
-                placeholder="Name (optional)"
-                onChange={(e) => setName(e.target.value)}
-                className={errors.name ? "form-bad" : "form-good"}
-              />
-            </label>
-            <p className="error">{errors.name}</p>
+      <h2>Upload an Item</h2>
+      <form onSubmit={onSubmit} encType="multipart/formdata">
+        <label>
+          Name
+          <input
+            type="text"
+            value={name}
+            placeholder="Name (optional)"
+            onChange={(e) => setName(e.target.value)}
+            className={errors.name ? "form-bad" : "form-good"}
+          />
+        </label>
+        <p className="error">{errors.name}</p>
 
-            {localFile && MatchElementToItem(file, localFile)}
-            <label>
-              <span>
-                File
-                <span className="req">*</span>
-              </span>
-              <input type="file" onChange={onFileChange} />
-            </label>
-            <p className="error">{errors.file}</p>
-            <div id="is-main-container">
-              <p>Is this item the main item of the gallery?</p>
-              <label>
-                <input
-                  type="radio"
-                  value={true}
-                  name="isMain"
-                  checked={isMain === true}
-                  onChange={() => setIsMain(true)}
-                />
-                Yes
-              </label>
-              <label>
-                <input
-                  type="radio"
-                  value={false}
-                  name="isMain"
-                  checked={isMain === false}
-                  onChange={() => setIsMain(false)}
-                />
-                No
-              </label>
-            </div>
+        {localFile && MatchElementToItem(file, localFile)}
+        <label>
+          <span>
+            File
+            <span className="req">*</span>
+          </span>
+          <input type="file" onChange={onFileChange} />
+        </label>
+        <p className="error">{errors.file}</p>
+        <div id="is-main-container">
+          <p>Is this item the main item of the gallery?</p>
+          <label>
+            <input
+              type="radio"
+              value={true}
+              name="isMain"
+              checked={isMain === true}
+              onChange={() => setIsMain(true)}
+            />
+            Yes
+          </label>
+          <label>
+            <input
+              type="radio"
+              value={false}
+              name="isMain"
+              checked={isMain === false}
+              onChange={() => setIsMain(false)}
+            />
+            No
+          </label>
+        </div>
 
-            <p>
-              <span className="req">*</span> indicates required fields
-            </p>
-            <button disabled={Object.keys(errors).length}>Upload Item</button>
-          </form>
-        </>
-      ) : (
-        <>
-          <h2>Uploading</h2>
-          <p>Please do not navigate away from this page...</p>
-          <div className="progress-bar-container">
-            <span>{uploadProgress}%</span>
-            <progress
-              className="progress-bar"
-              max="100"
-              value={`${smoothProgress}`}
-            >
-              {smoothProgress}%
-            </progress>
-          </div>
-        </>
-      )}
+        <p>
+          <span className="req">*</span> indicates required fields
+        </p>
+
+        <button disabled={Object.keys(errors).length}>Upload Item</button>
+      </form>
     </>
   );
 }
