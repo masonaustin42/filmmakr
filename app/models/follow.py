339218378f391ecf6efca1c77db480ed0711b0f3
masonaustin42 <masonaustin42@gmail.com>
2024-01-05 18:15:36 -0800

    add prefix for prod on Follow model

--- app/models/follow.py
+++ app/models/follow.py
@@ -1,4 +1,4 @@
-from .db import db, environment, SCHEMA
+from .db import db, environment, SCHEMA, add_prefix_for_prod
 
 class Follow(db.Model):
     __tablename__ = 'follows'
@@ -7,8 +7,8 @@ class Follow(db.Model):
         __table_args__ = {'schema': SCHEMA}
         
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    following_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), nullable=False)
+    following_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), nullable=False)
     
     user = db.relationship('User', back_populates='follows', foreign_keys=[user_id], uselist=False)
     following = db.relationship('User', back_populates='following', foreign_keys=[following_id], uselist=False)
