0094cfde3af2c77bb81723f18bb857679eb99c83
masonaustin42 <masonaustin42@gmail.com>
2024-01-08 11:43:01 -0800

    add follows seeders

---
+++ app/seeds/follows.py
@@ -0,0 +1,23 @@
+from app.models import db, Follow, environment, SCHEMA
+from sqlalchemy.sql import text
+import csv
+
+def seed_follows():
+    with open("app/seeds/follows.csv", "r") as file:
+        csvreader = csv.reader(file)
+        fields = next(csvreader)
+        for row in csvreader:
+            follow = Follow(
+                follower_id = int(row[0]),
+                followed_id = int(row[1])
+            )
+            db.session.add(follow)
+        db.session.commit()
+        
+def undo_follows():
+    if environment == "production":
+        db.session.execute(f"TRUNCATE table {SCHEMA}.follows RESTART IDENTITY CASCADE;")
+    else:
+        db.session.execute(text("DELETE FROM follows"))
+        
+    db.session.commit()
